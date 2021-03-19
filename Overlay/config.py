3e0b1d1537225b6890f58bc5db1d4ee627bcc93e
Dogson <dogsonkrul@gmail.com>
2021-03-19 13:08:01 +0100

    code refactoring

--- Overlay/config.py
+++ Overlay/config.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 import json
+import os
+import sys
 
 
 ROOT = tk.Tk()
 
 with open("data//settings.json", "r") as file:
     SETTINGS_DATA = json.load(file)
+
+if not os.path.exists("Videos"):
+    os.mkdir("Videos")
+
+if not os.path.exists("data//open_hardware_monitor//OpenHardwareMonitorLib.dll"):
+    input("Cannot find 'data//open_hardware_monitor//OpenHardwareMonitorLib.dll'")
+    sys.exit()
